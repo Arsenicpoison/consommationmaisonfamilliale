13dfa9f2ec1d09f4c27b3d4d1894ba06117dfdcb
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-02 00:22:22 +0200

    add acheter migrations

---
+++ app/models/acheter.py
@@ -0,0 +1,8 @@
+from django.db import models
+from app.models import Category, Product
+
+class Acheter(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total = models.FloatField()
