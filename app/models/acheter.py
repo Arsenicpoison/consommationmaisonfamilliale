1574baf91c0a90d636ac389fcc3ba7070a40adb6
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-02 00:56:51 +0200

    add date field in acheter model

--- app/models/acheter.py
+++ app/models/acheter.py
@@ -1,8 +1,10 @@
 from django.db import models
+import datetime
 from app.models import Category, Product
 
 class Acheter(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    date_achat = models.DateTimeField(default=datetime.datetime.now)
     total = models.FloatField()
