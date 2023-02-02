4eac6e77ab8e88a3aa64212815d1966276c31cfe
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-03 00:13:50 +0200

    update home view

--- app/views/home.py
+++ app/views/home.py
@@ -1,8 +1,26 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from app.models import Prevision,Acheter
 
 def index(request):
+    total_prevision=Prevision.objects.count()
+    total_acheter=Acheter.objects.count()
+    total=totalModel1(Prevision.objects.all())
+    totale =totalModel2(Acheter.objects.all())
     return render(
         request,
-        'app/home/index.html'
+        'app/home/index.html',
+        {
+            'total_prevision': total_prevision,
+            'total_acheter':total_acheter,
+            'total': total,
+            'totale': totale
+
+        }
     )
+def totalModel1(models):
+  return sum(model.total for model in models)
+
+def totalModel2(models):
+    return sum(model.total for model in models)
+   
